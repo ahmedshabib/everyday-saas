a8773a874409c882846a8afb9ea2503d22993cd6
ahmedshabib <reachshabib@gmail.com>
2020-07-26 15:09:58 +0530

    First Commit

--- apps/html-to-pdf/src/app/app.module.ts
+++ apps/html-to-pdf/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslocoRootModule } from './transloco-root.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, TranslocoRootModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ transloco.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rootTranslationsPath: 'apps/html-to-pdf/src/assets/i18n/',
+  langs: ['en', 'es'],
+  keysManager: {}
+};
